dec42d60a4d826f2d0e0cda1d94df9cdbb1db83b
YKKaya <73110464+YKKaya@users.noreply.github.com>
2023-11-13 12:58:54 +0100

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -20,41 +20,17 @@ if st.button("Generate Requirements"):
         # Process text with spaCy
         doc = nlp(text_input)
 
-        # Extract entities and dependencies
-        entities = {ent.text: ent.label_ for ent in doc.ents}
-        dependencies = {token.text: token.dep_ for token in doc}
-
-        # Determine roles dynamically
-        roles = set(entities.values())
-        if "USER" in roles:
-            user_role = "User"
-        else:
-            user_role = ""
-
-        if "ACTION" in roles:
-            action_role = "I want"
-        else:
-            action_role = ""
-
-        if "GOAL" in roles:
-            goal_role = "So that"
-        else:
-            goal_role = ""
+        # Extract sentences
+        sentences = [sent.text.strip() for sent in doc.sents]
 
         # Display user story format
         st.subheader("User Story:")
-        st.write(f"As a: {user_role}")
-        st.write(f"{action_role}:")
-        st.write(f"{goal_role}:")
-
-        st.subheader("Given:")
-        st.write("When:")
-        st.write("Then:")
+        for i, sentence in enumerate(sentences):
+            st.write(f"{sentence}:")
 
         # Display additional information
         st.subheader("Additional Information:")
-        st.write(f"Entities: {entities}")
-        st.write(f"Dependencies: {dependencies}")
+        st.write(f"Extracted Sentences: {sentences}")
 
     else:
         st.warning("Please enter some text.")
